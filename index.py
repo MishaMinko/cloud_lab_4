c7cb32361ea99b99f8c98889145ed26a7086dd87
Misha <Misha@DESKTOP-D1T401Q>
2025-11-14 15:42:39 +0200

    fix

--- index.py
+++ index.py
@@ -1,4 +1,29 @@
+import json
+
 def handler(request):
+    method = request.method
+
+    # GET: читаємо query параметр ?name=
+    if method == "GET":
+        name = request.query_params.get("name", "Anonymous")
+        return {
+            "message": f"Hello, {name}!"
+        }
+
+    # POST: читаємо JSON тіло
+    if method == "POST":
+        try:
+            body = request.body.decode()
+            data = json.loads(body)
+            name = data.get("name", "Anonymous")
+            return {
+                "message": f"POST received from {name}"
+            }
+        except:
+            return {
+                "error": "Invalid JSON"
+            }
+
     return {
-        "message": "Hello from Vercel Python!"
+        "error": "Unsupported method"
     }
