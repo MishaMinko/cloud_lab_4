80363086ee72fd10b41b6ffea43ab4b1daac34af
Misha <Misha@DESKTOP-D1T401Q>
2025-11-28 15:50:50 +0200

    smaller code

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from urllib.parse import urlparse, parse_qs
 
@@ -6,11 +6,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         query = parse_qs(urlparse(self.path).query)
         name = query.get("name", ["Anonymous"])[0]
-
-        response = {
-            "message": f"Hello, {name}!"
-        }
-
+        response = {"message": f"Hello, {name}!"}
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -25,13 +21,11 @@ class handler(BaseHTTPRequestHandler):
             response = {"message": f"POST received from {name}"}
         except:
             response = {"error": "Invalid JSON"}
-
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(response).encode("utf-8"))
 
 if __name__ == "__main__":
-    from http.server import HTTPServer
     server = HTTPServer(("0.0.0.0", 8000), handler)
     server.serve_forever()
